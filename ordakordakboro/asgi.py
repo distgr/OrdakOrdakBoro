e726880c173db0fca520be1faaef17509580f8cb
Matt <callmethematt@gmail.com>
2022-01-12 10:35:35 +0330

    Matching the project with Django 4.0.1

--- ordakordakboro/asgi.py
+++ ordakordakboro/asgi.py
@@ -1,7 +1,7 @@
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ordakordakboro.settings')
 
-application = get_wsgi_application()
+application = get_asgi_application()
